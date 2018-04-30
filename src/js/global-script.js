47623eb6f3572877e98e1a3dac766be645d9a7e6
amstelart <amsteldroid@gmail.com>
2018-04-30 07:14:39 +0300

    сдача проекта

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,18 +1,18 @@
 // Если на проекте jQuery
 $( document ).ready(function() {
   // code
-  $('#main-slider').sliderPro({
-    width: '100%',
-    height: 620,
-    arrows: true,
-    buttons: true,
-    waitForLayers: true,
-    autoplay: false,
-    autoScaleLayers: false,
-    breakpoints: {
-      500: {
-      }
-    }
+
+  $('.main-slider').owlCarousel({
+      items:1,
+      loop:true,
+      nav:true,
+      navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+      dots:true,
+      dotsSpeed:650,
+      autoplay:true,
+      autoplayTimeout:6000,
+      autoplayHoverPause:true,
+      autoplaySpeed:1000
   });
 
   $('#review-carousel').owlCarousel({
