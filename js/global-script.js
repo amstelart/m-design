ef40ea665c99a5689e24d6b581b4bf1170ff71ae
amstelart <amsteldroid@gmail.com>
2018-04-30 07:16:26 +0300

    Update 2018-04-30T04:16:22.356Z

---
+++ js/global-script.js
@@ -0,0 +1,46 @@
+// Если на проекте jQuery
+$( document ).ready(function() {
+  // code
+
+  $('.main-slider').owlCarousel({
+      items:1,
+      loop:true,
+      nav:true,
+      navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+      dots:true,
+      dotsSpeed:650,
+      autoplay:true,
+      autoplayTimeout:6000,
+      autoplayHoverPause:true,
+      autoplaySpeed:1000
+  });
+
+  $('#review-carousel').owlCarousel({
+    items: 1,
+    loop:true,
+    nav: true,
+    dots: true,
+    smartSpeed: 700,
+    autoplayHoverPause: true,
+    margin:0,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
+  });
+});
+
+// Изоляция без jQuery
+// (function(){
+//   // code
+// }());
+
+// На проекте нет jQuery, но хочется $( document ).ready...
+// function ready(fn) {
+//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+//
+// ready(function(){
+//   // code
+// });
